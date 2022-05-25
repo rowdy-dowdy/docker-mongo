eb17b703b553cbc9bf3f04c28224c4eedf3bcff7
Viet Hung <viet.hung.2898@gmail.com>
2022-05-25 16:03:22 +0700

    add

---
+++ mongo/mongo-entrypoint/init.js
@@ -0,0 +1,2 @@
+// db.createUser({user: 'nuxt_nuxt', pwd: '123456', roles: [{role: 'readWrite', db: 'nuxt'}]});
+// db.createUser({user: 'admin', pwd: '123456', roles: [{role: 'userAdminAnyDatabase', db: 'admin'}]});
